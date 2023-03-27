8255935b72d7c670a2bba8c61b9c3ebc143b71ec
farouksouei <farouksouei@gmail.com>
2023-03-27 12:24:13 +0100

    switching processes between cpu and gpu

--- tensorDataTypes.py
+++ tensorDataTypes.py
@@ -31,10 +31,29 @@ import numpy as np
 # print(y)
 # print(y.dtype)
 
-x = np.zeros((2, 2), dtype=np.float32)
-print(x)
-print(x.dtype)
+# x = np.zeros((2, 2), dtype=np.float32)
+# print(x)
+# print(x.dtype)
 
-y = torch.from_numpy(x)
-print(y)
-print(y.dtype)
+# y = torch.from_numpy(x)
+# print(y)
+# print(y.dtype)
+
+# Moving tensors to GPU
+
+x = torch.tensor([1.5, 2])
+print(x)
+print(x.device)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    x = x.to(device)
+    print(x)
+    print(x.device)
+
+else:
+    print("CUDA is not available.")
+    device = torch.device("cpu")
+    x = x.to(device)
+    print(x)
+    print(x.device)
